fe30f5edefad8c25b236796f9b6a54e83319888f
TheRoyalTnetennba <paye@google.com>
2019-03-18 13:22:41 -0700

    rebuild build scripts

--- .kokoro/osx.sh
+++ .kokoro/osx.sh
@@ -29,16 +29,29 @@ versions=(2.3.8 2.4.5 2.5.5 2.6.2)
 source /Users/kbuilder/.rvm/scripts/rvm
 
 if [ "$JOB_TYPE" = "presubmit" ]; then
-    (rvm use ${versions[2]} --default) || (rvm install ${versions[2]} && rvm use ${versions[2]} --default)
+    (rvm use ${versions[2]}@global --default) || (rvm install ${versions[2]}@global && rvm use ${versions[2]}@global --default)
+    echo $PATH
+    which bundler
+    which ruby
     gem install bundler --version 1.17.3
+    echo $PATH
+    which bundler
+    which ruby
     gem update --system
-    gem regenerate_binstubs
+    echo $PATH
+    which bundler
+    which ruby
+    gem pristine --binstubs
+    echo $PATH
+    which bundler
+    which ruby
     ruby --version
-    which bundle
+    echo $PATH
+    which bundler
     (bundle update && bundle exec rake kokoro:presubmit) || set_failed_status
 else
     for version in "${versions[@]}"; do
-        (rvm use "$version" --default) || (rvm install "$version" && rvm use "$version" --default)
+        (rvm use "$version"@global --default) || (rvm install "$version" && rvm use "$version"@global --default)
         git fetch --depth=10000
         gem install bundler --version 1.17.3
         gem update --system
